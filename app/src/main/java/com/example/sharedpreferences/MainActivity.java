841b5af89170b2e0cbdb8911a52c4629f9c727aa
seoyoon04 <sykim8483@naver.com>
2021-06-12 01:33:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et_save;
+    String shared = "file";
+
+
+    //불러오기 구문
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_save = (EditText)findViewById(R.id.et_save);
+
+
+    }
+}
